8842ea542f173dc82c1d204e16bbf45f4d9db39f
Gabriel <gabriel.castro@castrom.net>
2026-07-20 09:45:16 -0300

    Criamos um pacote medico para guardamos tudo que for relacionado a medicos; Criamos uma Record chamada DadosCadastroMedicos para guaradar as informações recebidas da API; Criamos uma Enum chamada Especialidade que contém as especialidades dos médicos; Criamos uma package endereco, e dentro dela criamos outra record chamada DadosEndereco, que serve para guardas as informações do endereço do médico como: Logradouro, CEP e etc...

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import med.voll.api.medico.DadosCadastroMedicos;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
 
     @PostMapping        //esse @RequestBody serve para puxar o json do corpo da requisição
-    public void cadastar(@RequestBody String json) {
-        System.out.println(json);
+    public void cadastar(@RequestBody DadosCadastroMedicos dados) {
+        System.out.println(dados);
 
     }
 }
