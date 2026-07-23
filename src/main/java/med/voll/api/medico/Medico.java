f2f972402cbe05135363d7ecbb68c4c3affdb537
Gabriel <gabriel.castro@castrom.net>
2026-07-23 11:17:35 -0300

    exclusão lógica de médicos(sem exclusão, apenas inativação)

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -32,7 +32,10 @@ public class Medico {
    @Embedded
     private Endereco endereco;
 
+   private boolean ativo;
+
     public Medico(DadosCadastroMedicos dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -52,5 +55,9 @@ public class Medico {
             this.endereco.atualizarInformacoes(dados.endereco());
         }
 
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
